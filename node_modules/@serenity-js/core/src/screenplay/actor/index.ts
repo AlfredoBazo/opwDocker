6eec8ae639d2f1648c20d8e92339b83481729f07
lcaparros <luiscaparrosruiz@gmail.com>
2021-04-30 13:46:05 +0200

    Removed unused files

--- node_modules/@serenity-js/core/src/screenplay/actor/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Actor';
-export * from './AnswersQuestions';
-export * from './CanHaveAbilities';
-export * from './CollectsArtifacts';
-export * from './ListensToDomainEvents';
-export * from './PerformsActivities';
-export * from './UsesAbilities';
--- node_modules/@serenity-js/cucumber/src/listeners/legacy/cucumber-1.ts
+++
@@ -1,1 +0,0 @@
-export = require('./cucumber-0'); // tslint:disable-line:no-var-requires
