a2d75e97fdd58b54b7219d39d225e9263e532c63
AlfredoBazo <alfredo.bazo89@gmail.com>
2021-05-01 09:59:37 +0200

    Update protractor.conf.js

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -15,17 +15,17 @@ exports.config = {
   //         browser.driver.manage().window().setSize(result.width, result.height);
   //       });
   // },
-  plugins: [
-    {
-      package: require.resolve('protractor-multiple-cucumber-html-reporter-plugin'),
-      options: {
-        removeExistingJsonReportFile: true,
-        removeOriginalJsonReportFile: true,
-        automaticallyGenerateReport: true,
-        openReportInBrowser: true
-      }
-    }
-  ],
+//   plugins: [
+//     {
+//       package: require.resolve('protractor-multiple-cucumber-html-reporter-plugin'),
+//       options: {
+//         removeExistingJsonReportFile: true,
+//         removeOriginalJsonReportFile: true,
+//         automaticallyGenerateReport: true,
+//         openReportInBrowser: true
+//       }
+//     }
+//   ],
 
   allScriptsTimeout: 11000,
 
@@ -35,15 +35,15 @@ exports.config = {
     browserName: 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/',
+  //baseUrl: 'http://localhost:8000/',
 
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'), // Here it is
 
   cucumberOpts: {
-    format: [require.resolve('cucumber-pretty'), 'json: results.json'],
+    format: [require.resolve('cucumber-pretty')],
     require: '../e2e-tests/src/step_definitions/mysteps.js' // This is where we'll be writing our actual tests
   },
 
-  resultJsonOutputFile: './testResults.json', //output file path to store the final results in .json format
+ // resultJsonOutputFile: './testResults.json', //output file path to store the final results in .json format
 };
