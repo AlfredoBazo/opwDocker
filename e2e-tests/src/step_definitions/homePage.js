3c02aed65d61d46230986836fc722f84b3420eff
AlfredoBazo <alfredo.bazo89@gmail.com>
2021-05-03 17:39:44 +0200

    Update homePage.js

--- e2e-tests/src/step_definitions/homePage.js
+++ e2e-tests/src/step_definitions/homePage.js
@@ -11,10 +11,10 @@ const ecTimeout = 15000;
 const log4js = require('log4js');
 log4js.configure({
     appenders: {
-        out: { type: 'stdout' }
+        out: {type: 'stdout'}
     },
     categories: {
-        default: { appenders: ['out'], level: browser.params.level ? browser.params.level : 'INFO' }
+        default: {appenders: ['out'], level: browser.params.level ? browser.params.level : 'INFO'}
     }
 });
 
@@ -22,24 +22,65 @@ var logger = log4js.getLogger();
 
 module.exports = {
     /**
-     * Function to open the main page of the UI.
-     * @param product
+     * Función para abrir la web principal de angular.
      * @param callback
      */
     angularPage: function (callback) {
-
         browser.get(pageObject.homePage.url).then(function () {
-            logger.info('Navegamos a: https://angular.io/ ' );
+            logger.info('Navegamos a: https://angular.io/ ');
             callback(); // To tell the cucumber that we are done with thi
         });
     },
 
 
-
-    urlOpened: function(callback) {
-        browser.wait(protractor.ExpectedConditions.urlContains(pageObject.homePage.url), ecTimeout).then(function() {
+    /**
+     * Función para comprobar que se ha abierto la web de angular.
+     * @param callback
+     */
+    urlOpened: function (callback) {
+        browser.wait(protractor.ExpectedConditions.urlContains(pageObject.homePage.url), ecTimeout).then(function () {
             logger.info('Web de ' + pageObject.homePage.url + 'abierta');
             callback();
         });
     },
+
+    /**
+     * Función para hacer click en una sección
+     * @param seccion
+     * @param callback
+     */
+    clickSeccion: function (seccion, callback) {
+        browser.wait(protractor.ExpectedConditions.elementToBeClickable(pageObject.homePage.boton[seccion]), ecTimeout).then(function () {
+            pageObject.homePage.boton[seccion].click().then(function () {
+                logger.info("La seccion" + seccion + "ha sido pulsada.");
+                callback();
+            });
+        });
+    },
+
+    /**
+     * Función para comprobar que una sección se ha abierto
+     * @param seccion
+     * @param callback
+     */
+    urlSeccion: function (seccion, callback) {
+        browser.wait(protractor.ExpectedConditions.urlContains(seccion)).then(function () {
+            logger.info("La sección de " + seccion + "ha sido abierta");
+            callback();
+        });
+    },
+
+    /**
+     * Función para introducir una palabra en el buscador.
+     * @param palabra
+     * @param callback
+     */
+    buscarPalabra: function (palabra, callback) {
+        browser.wait(protractor.ExpectedConditions.visibilityOf(pageObject.homePage.search), ecTimeout).then(function () {
+            pageObject.homePage.search.sendKeys(palabra).then(function () {
+                logger.info("Palabra " + palabra + "insertada en el buscador");
+                callback();
+            })
+        });
+    }
 };
