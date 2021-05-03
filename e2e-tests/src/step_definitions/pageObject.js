a7c1af5de35e0151a390a7c6ebe5c86dcccd0cdc
AlfredoBazo <alfredo.bazo89@gmail.com>
2021-05-03 17:46:10 +0200

    Update pageObject.js

--- e2e-tests/src/step_definitions/pageObject.js
+++ e2e-tests/src/step_definitions/pageObject.js
@@ -4,7 +4,10 @@ module.exports = {
         url: "https://angular.io/",
         boton: {
             start: element(by.css('[href="docs"]')),
-            features: element(by.css('[href="features"]'))
+            features: element(by.css('[href="features"]')),
+            resources: element(by.css('[href="resources"]')),
+            events: element(by.css('[href="events"]'))
         },
+        search: element(by.css('[aria-label="search"]'))
     }
 };
