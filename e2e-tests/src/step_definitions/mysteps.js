e0a333c98ffc9761d25da195f5c3d0612cb2ea7c
AlfredoBazo <alfredo.bazo89@gmail.com>
2021-05-03 17:39:18 +0200

    Update mysteps.js

--- e2e-tests/src/step_definitions/mysteps.js
+++ e2e-tests/src/step_definitions/mysteps.js
@@ -1,5 +1,5 @@
 "use strict";
-let { Given, When, Then } = require('cucumber');
+let {Given, When, Then} = require('cucumber');
 let homePage = require('./homePage.js');
 let utils = require('./../Utils/utils.js');
 const properties = require('./pageObject');
@@ -11,4 +11,13 @@ When(/^el usuario abre la web de angular$/, function (callback) {
 
 Then(/^la web abierta es la correcta$/, function (callback) {
     homePage.urlOpened(callback);
+});
+When(/^abrimos la seccion (.*)$/, function (seccion, callback) {
+    homePage.clickSeccion(seccion, callback);
+});
+Then(/^se abre la seccion (.*)$/, function (seccion, callback) {
+    homePage.urlSeccion(seccion,callback);
+});
+When(/^buscamos la palabra (.*)$/, function (palabra, callback) {
+    homePage.buscarPalabra(palabra,callback);
 });
