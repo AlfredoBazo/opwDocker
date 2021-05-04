5b01eede251b29045d34187350644cb816f9960a
AlfredoBazo <alfredo.bazo89@gmail.com>
2021-05-04 13:58:09 +0200

    Update protractor.conf.js

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -6,31 +6,6 @@ exports.config = {
     browser.driver.manage().window().maximize();
 },
 
-//   onPrepare() {
-//     browser.driver
-//         .executeScript(function() {
-//           return {
-//             width: window.screen.availWidth,
-//             height: window.screen.availHeight
-//           };
-//         })
-//         .then(function(result) {
-//           browser.driver.manage().window().setPosition(0, 0);
-//           browser.driver.manage().window().setSize(result.width, result.height);
-//         });
-//   },
-//   plugins: [
-//     {
-//       package: require.resolve('protractor-multiple-cucumber-html-reporter-plugin'),
-//       options: {
-//         removeExistingJsonReportFile: true,
-//         removeOriginalJsonReportFile: true,
-//         automaticallyGenerateReport: true,
-//         openReportInBrowser: true
-//       }
-//     }
-//   ],
-
   allScriptsTimeout: 11000,
 
   specs: ['../e2e-tests/src/Features/*.feature'],
@@ -51,5 +26,4 @@ exports.config = {
     require: '../e2e-tests/src/step_definitions/mysteps.js' // This is where we'll be writing our actual tests
   },
 
- // resultJsonOutputFile: './testResults.json', //output file path to store the final results in .json format
 };
